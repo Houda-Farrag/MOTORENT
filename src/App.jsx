60091dc57517affbec49454aea36125b64384826
= <=>
2024-03-05 03:38:10 +0200

    add- signUp functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Register from "../src/pages/Register";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import CarCard from "./components/CarCard/CarCard";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "react-hot-toast";
 
 const router = createBrowserRouter([
   {
@@ -23,8 +26,41 @@ const router = createBrowserRouter([
   },
 ]);
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    },
+  },
+});
+
 function App() {
-  return <RouterProvider router={router}></RouterProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <Toaster
+        position="top-center"
+        gutter={12}
+        containerStyle={{ margin: "8px" }}
+        toastOptions={{
+          success: {
+            duration: 5000,
+          },
+          error: {
+            duration: 5000,
+          },
+          style: {
+            fontSize: "16px",
+            maxWidth: "500px",
+            padding: "16px 24px",
+            backgroundColor: "var(--color-grey-0)",
+            color: "var(--color-grey-700)",
+          },
+        }}
+      />
+      <RouterProvider router={router}></RouterProvider>;
+    </QueryClientProvider>
+  );
 }
 
 export default App;
---
+++ src/pages/Auth/useSignup.js
@@ -0,0 +1,28 @@
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "react-hot-toast";
+import { signup as signupApi } from "../../service/userApi";
+import { useNavigate } from "react-router-dom";
+
+export function useSignup() {
+  const navigate = useNavigate();
+  const {
+    mutate: signup,
+    isLoading,
+    data,
+    isError,
+  } = useMutation({
+    mutationFn: signupApi,
+    onSuccess: (user) => {
+      toast.success(
+        "Account successfully created! Please verify the new account from the user's email address."
+      );
+      navigate("/login");
+    },
+    onError: () => {
+      toast.error("Sign up is not possible , please try agian later");
+      console.log(isError);
+    },
+  });
+
+  return { signup, isLoading, data };
+}
