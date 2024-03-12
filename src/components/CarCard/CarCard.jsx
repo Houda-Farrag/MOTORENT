d5837917e718a853fbfb53a1b297815f96edcd6d
mohamed <mohamedzakaria0006@gmail.com>
2024-03-12 13:52:10 +0200

    cardetails

--- src/components/CarCard/CarCard.jsx
+++ src/components/CarCard/CarCard.jsx
@@ -19,76 +19,76 @@ import AdjustIcon from "@mui/icons-material/Adjust";
 import PersonIcon from "@mui/icons-material/Person";
 import FavouriteICon from "../../ui/FavouriteICon";
 
-function CarCard(){
-  // const {category , images , tankCapacity , transmission , capacity} = cars.data[0]
-  return (
-    <Card
-    sx={{
-      maxWidth: "304px",
-      maxHeight: "478px",
-      padding: "10px !important",
-    }}
-    className={styles.slider}
-  >
-    <CardHeader action={<FavouriteICon />} title="sedan" />
-    <Box
-      sx={{
-        margin: "30px 0px",
-      }}
-    >
-      <CardMedia
-        component="img"
-        // image={images[0].url}
-        image = './public/car.png'
-        alt="Paella dish"
-        width="100%"
-        height="100%"
-        sx={{
-          marginTop: "15px",
-          marginBottom: "15px",
-        }}
-      />
-       <Box className={styles.shadow}></Box> 
-    </Box>
-    <CardContent>
+// function CarCard(){
+//   // const {category , images , tankCapacity , transmission , capacity} = cars.data[0]
+//   return (
+//     <Card
+//     sx={{
+//       maxWidth: "304px",
+//       maxHeight: "478px",
+//       padding: "10px !important",
+//     }}
+//     className={styles.slider}
+//   >
+//     <CardHeader action={<FavouriteICon />} title="sedan" />
+//     <Box
+//       sx={{
+//         margin: "30px 0px",
+//       }}
+//     >
+//       <CardMedia
+//         component="img"
+//         // image={images[0].url}
+//         image = './public/car.png'
+//         alt="Paella dish"
+//         width="100%"
+//         height="100%"
+//         sx={{
+//           marginTop: "15px",
+//           marginBottom: "15px",
+//         }}
+//       />
+//        <Box className={styles.shadow}></Box> 
+//     </Box>
+//     <CardContent>
 
-      <Box display="flex" justifyContent="space-between" width="100%" color="#90A3BF">
-        <Box display="flex">
-          <EvStationIcon fontSize="small" />
-          <Typography>80 L</Typography>
-        </Box>
-        <Box display="flex">
-          <AdjustIcon fontSize="small" />
-          <Typography>auto</Typography>
-        </Box>
-        <Box display="flex">
-          <PersonIcon fontSize="small" />
-          <Typography>2 persons</Typography>
-        </Box>
-      </Box>
-    </CardContent>
-    <CardContent>
-      <Box
-        display="flex"
-        justifyContent="space-between"
-        alignItems="center"
-      >
-        <Box>
-          $<strong>99.00</strong>/day
-        </Box>
-        <Box>
-          <Button variant="contained">Rent NOW</Button>
-        </Box>
-      </Box>
-    </CardContent>
-    <CardActions disableSpacing></CardActions>
-  </Card>
-  )
-}
+//       <Box display="flex" justifyContent="space-between" width="100%" color="#90A3BF">
+//         <Box display="flex">
+//           <EvStationIcon fontSize="small" />
+//           <Typography>80 L</Typography>
+//         </Box>
+//         <Box display="flex">
+//           <AdjustIcon fontSize="small" />
+//           <Typography>auto</Typography>
+//         </Box>
+//         <Box display="flex">
+//           <PersonIcon fontSize="small" />
+//           <Typography>2 persons</Typography>
+//         </Box>
+//       </Box>
+//     </CardContent>
+//     <CardContent>
+//       <Box
+//         display="flex"
+//         justifyContent="space-between"
+//         alignItems="center"
+//       >
+//         <Box>
+//           $<strong>99.00</strong>/day
+//         </Box>
+//         <Box>
+//           <Button variant="contained">Rent NOW</Button>
+//         </Box>
+//       </Box>
+//     </CardContent>
+//     <CardActions disableSpacing></CardActions>
+//   </Card>
+//   )
+// }
 
-export default CarCard;
+// export default CarCard;
 
-/*
+
 function CarCard() {
   return (
     <div className={styles.slider}>
@@ -162,5 +162,5 @@ function CarCard() {
 }
 
 export default CarCard;
-*/
+
 
---
+++ src/components/FilledFavHeart/FilledFavHeart.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import IconButton from '@mui/material/IconButton';
+
+
+const FilledFavHeart=()=>{
+    return<>
+    <IconButton aria-label="fingerprint" color="red">
+      <FavoriteIcon/>
+      </IconButton>
+    </>
+}
+export default FilledFavHeart;
