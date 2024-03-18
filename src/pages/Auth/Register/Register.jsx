92ccb375bab716f896d04455a1accc8c20010df5
mohamed <mohamedzakaria0006@gmail.com>
2024-03-18 08:24:22 +0200

    profile-data

--- src/pages/Auth/Register/Register.jsx
+++ src/pages/Auth/Register/Register.jsx
@@ -8,7 +8,7 @@ import LoadingIndicator from "../../../ui/LoadingIndicator";
 // import { useSignup } from "../Register/useSignup";
 
 const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/gi;
-const passwordRegex = /^[A-Z][a-z0-9]{8,}$/gi;
+const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&.])[A-Za-z\d@$!%*?&.]+$/
 
 function Register() {
   const navigate = useNavigate();
